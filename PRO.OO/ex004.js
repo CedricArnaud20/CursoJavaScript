bf77531dc075f1fa038c7e3ebd3ae61cdcd9e1fa
cedric arnaud <79386450+CedricArnaud20@users.noreply.github.com>
2024-03-14 21:30:58 -0300

    OO EXO

---
+++ PRO.OO/ex004.js
@@ -0,0 +1,37 @@
+class Carro { 
+    constructor(marca, cor, gasolinaRestante, consumo){
+        this.marca = marca;
+        this.cor = cor;
+        this.gasolinaRestante = gasolinaRestante
+        this.consumo = consumo;
+    }
+
+
+    Dirigir(km){
+
+        let litrosConsumidos = km / this.consumo;
+
+        this.gasolinaRestante -= litrosConsumidos;
+
+    }
+
+    Abaster(Gas){
+        let  tanque = Gas ;
+
+        this.gasolinaRestante += tanque;
+
+    }
+
+}
+
+
+
+let carro = new Carro("Fiat","Vemelho",100 ,10);
+
+// console.log(carro);
+
+carro.Dirigir(100)
+
+// carro.Abaster(200)
+
+console.log(carro);
