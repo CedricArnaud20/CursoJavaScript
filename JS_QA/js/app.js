cdae57478df7f0ce01099a5d75d94e4b843ae783
cedric arnaud <79386450+CedricArnaud20@users.noreply.github.com>
2024-11-03 17:09:41 -0300

    novos arquivos

---
+++ JS_QA/js/app.js
@@ -0,0 +1,25 @@
+// var saldo = 1000 
+
+// function saque(valor) {
+
+//     if (valor > saldo) {
+//         console.log('valor is less than saldo')
+//     }else if(valor > 700 ){
+//         console.log('saque valor is more than the limit')
+//     }
+//     else(
+//         saldo = saldo - valor 
+//     )
+       
+// }
+
+
+// saque(701)
+// console.log(saldo)
+
+//  Arrays //////////////////////////////////
+
+
+var gaveteiro = ['Meias', 'Gravatas', 'Documentos', 'Salgadinho']
+
+
